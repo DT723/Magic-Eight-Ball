d94236c12f9fe4e1c8df11241238963df36eeabc
DT723 <davidtesno@gmail.com>
2022-02-20 22:18:01 -0500

    added header + styling

--- script.js
+++ script.js
@@ -3,92 +3,96 @@
 ///////////////////////////////
 //MAGIC 8 BALL
 ///////////////////////////////
+const holder = document.querySelector('.message-holder');
+const message = document.querySelector('#message');
+const ball = document.querySelector('.ball');
 
-//ask for username with prompt
-let userName = prompt(`What is your name?`);
-// console.log(userName);
-
-//Check if username exists - display a message
-if (userName) {
-  console.log(`Hello ${userName}, ready to play magic eight ball?`);
-} else {
-  prompt(`Please enter a name`);
-}
-
-//create variable and store the value of the question
-let userQuestion = prompt(`Please ask a question, no question mark needed`);
-//letting the user know what question they asked
-console.log(`${userName}, You have asked; ${userQuestion}?`);
-
-//generate random number
 let randomNumber = Math.floor(Math.random() * 20) + 1;
 console.log(randomNumber);
 
-//create the 8 ball
+//Eight ball animation
+fadeInAnimation = function () {
+  setTimeout(function () {
+    ball.classList.remove('shake');
+    holder.style.opacity = 1;
+    message.style.opacity = 1;
+  }, 4000);
+};
+
+fadeOutAnimation = function (callback) {
+  holder.style.opacity = 0;
+  message.style.opacity = 0;
+  callback();
+};
+
+ball.addEventListener('click', function () {
+  ball.classList.add('shake');
+  fadeOutAnimation(fadeInAnimation);
+});
+
+// create the 8 ball
 let eightBall = ' ';
 
-//Create 8 ball responses based on randomNumber
+// Create 8 ball responses based on randomNumber
 switch (randomNumber) {
   case 1:
-    alert(`It is certain.`);
+    eightBall = `It is certain.`;
     break;
   case 2:
-    alert(`As I see it, yes.`);
+    eightBall = `As I see it, yes.`;
     break;
   case 3:
-    alert(`Reply hazy, try again.`);
+    eightBall = `Reply hazy, try again.`;
     break;
   case 4:
-    alert(`Don't count on it.`);
+    eightBall = `Don't count on it.`;
     break;
   case 5:
-    alert(`It is decidedly so.`);
+    eightBall = `It is decidedly so.`;
     break;
   case 6:
-    alert(`Most likely.`);
+    eightBall = `Most likely.`;
     break;
   case 7:
-    alert(`Ask again later.`);
+    eightBall = `Ask again later.`;
     break;
   case 8:
-    alert(`My reply is no.`);
+    eightBall = `My reply is no.`;
     break;
   case 9:
-    alert(`My sources say no.`);
+    eightBall = `My sources say no.`;
     break;
   case 10:
-    alert(`Better not tell you now.`);
+    eightBall = `Better not tell you now.`;
     break;
   case 11:
-    alert(`Outlook good.`);
+    eightBall = `Outlook good.`;
     break;
   case 12:
-    alert(`Without a doubt.`);
+    eightBall = `Without a doubt.`;
     break;
   case 13:
-    alert(`Yes definitely.`);
+    eightBall = `Yes definitely.`;
     break;
   case 14:
-    alert(`Cannot predict now.`);
+    eightBall = `Cannot predict now.`;
     break;
   case 15:
-    alert(`Yes.`);
+    eightBall = `Yes.`;
     break;
   case 16:
-    alert(`Outlook not so good.`);
+    eightBall = `Outlook not so good.`;
     break;
   case 17:
-    alert(`Signs point to yes.`);
+    eightBall = `Signs point to yes.`;
     break;
   case 18:
-    alert(`Concentrate and ask again.`);
+    eightBall = `Concentrate and ask again.`;
     break;
   case 19:
-    alert(`Very doubtful.`);
+    eightBall = `Very doubtful.`;
     break;
   case 20:
-    alert(`You may rely on it.`);
+    eightBall = `You may rely on it.`;
     break;
 }
-//print magic eight ball's answer
-console.log(`The eightball answer is: ${eightBall}`);
