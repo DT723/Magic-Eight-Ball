bc80b92bc81d17f790dda7138b39c8a9fa2b5783
DT723 <davidtesno@gmail.com>
2022-02-21 19:37:58 -0500

    some css styles and js functions

--- script.js
+++ script.js
@@ -6,93 +6,57 @@
 const holder = document.querySelector('.message-holder');
 const message = document.querySelector('#message');
 const ball = document.querySelector('.ball');
+const userInput = document.getElementById('input');
+const messages = [
+  'It is certain',
+  'It is decidedly so',
+  'Without a doubt',
+  'Yes definitely',
+  'You may rely on it',
+  'As I see it, yes',
+  'Most likely',
+  'Outlook good',
+  'Yes',
+  'Signs point to yes',
+  'Reply hazy try again',
+  'Ask again later',
+  'Better not tell you now',
+  'Cannot predict now',
+  'Concentrate and ask again',
+  "Don't count on it",
+  'My reply is no',
+  'My sources say no',
+  'Outlook not so good',
+  'Very doubtful',
+];
 
-let randomNumber = Math.floor(Math.random() * 20) + 1;
-console.log(randomNumber);
+//check if user typed a question in input. if true, then call ball function
+const userQuestionCheck = () => {
+  // const userInput = userInput.value.toLowerCase(); // dont use the same name causes an error
+  const userInputValue = userInput.value.toLowerCase();
+  if (userInputValue.length > 0) {
+    ball.classList.add('shake');
+    fadeOutAnimation(fadeInAnimation);
+  } else {
+    alert('Please ask a question');
+  }
+};
 
 //Eight ball animation
-fadeInAnimation = function () {
+const fadeInAnimation = () => {
   setTimeout(function () {
     ball.classList.remove('shake');
     holder.style.opacity = 1;
     message.style.opacity = 1;
+    message.innerHTML = messages[Math.floor(Math.random() * messages.length)];
   }, 4000);
 };
+console.log(fadeInAnimation);
 
-fadeOutAnimation = function (callback) {
+const fadeOutAnimation = callback => {
   holder.style.opacity = 0;
   message.style.opacity = 0;
   callback();
 };
 
-ball.addEventListener('click', function () {
-  ball.classList.add('shake');
-  fadeOutAnimation(fadeInAnimation);
-});
-
-// create the 8 ball
-let eightBall = ' ';
-
-// Create 8 ball responses based on randomNumber
-switch (randomNumber) {
-  case 1:
-    eightBall = `It is certain.`;
-    break;
-  case 2:
-    eightBall = `As I see it, yes.`;
-    break;
-  case 3:
-    eightBall = `Reply hazy, try again.`;
-    break;
-  case 4:
-    eightBall = `Don't count on it.`;
-    break;
-  case 5:
-    eightBall = `It is decidedly so.`;
-    break;
-  case 6:
-    eightBall = `Most likely.`;
-    break;
-  case 7:
-    eightBall = `Ask again later.`;
-    break;
-  case 8:
-    eightBall = `My reply is no.`;
-    break;
-  case 9:
-    eightBall = `My sources say no.`;
-    break;
-  case 10:
-    eightBall = `Better not tell you now.`;
-    break;
-  case 11:
-    eightBall = `Outlook good.`;
-    break;
-  case 12:
-    eightBall = `Without a doubt.`;
-    break;
-  case 13:
-    eightBall = `Yes definitely.`;
-    break;
-  case 14:
-    eightBall = `Cannot predict now.`;
-    break;
-  case 15:
-    eightBall = `Yes.`;
-    break;
-  case 16:
-    eightBall = `Outlook not so good.`;
-    break;
-  case 17:
-    eightBall = `Signs point to yes.`;
-    break;
-  case 18:
-    eightBall = `Concentrate and ask again.`;
-    break;
-  case 19:
-    eightBall = `Very doubtful.`;
-    break;
-  case 20:
-    eightBall = `You may rely on it.`;
-    break;
-}
+ball.addEventListener('click', userQuestionCheck); // listens to click on click userQuestionCheck is being called
